bc1ff6de8374e52adfe2d75b3ba398217041c120
Geertje van Dijk <geertjevandijk70@gmail.com>
2024-02-06 12:07:32 +0100

    removed old projects

--- click-test/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BACKUPDIR = os.path.join("/", "backups", "sexgames")
-DATADIR = os.path.join("/", "data", "downloads", "SexGames")
-KEEPDIR = os.path.join(BACKUPDIR, "keep")
-CHECKDIR = os.path.join(BACKUPDIR, "checked")
-
-FOLDERS = [ BACKUPDIR, KEEPDIR, CHECKDIR, DATADIR ]
